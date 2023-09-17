0c03acbd18768faaa0185d6c7ce7cd73e0edc3ae
iren <irasibil2@gmail.com>
2023-09-17 16:27:57 +0300

    connected mongo-db

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
+
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const port = 3000
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+)
 
 const authRoute = require('./routes/auth')
 
