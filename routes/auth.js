73d96ca77bf25cc85cbf5f812194056b1b3481e3
iren <irasibil2@gmail.com>
2023-09-17 20:10:45 +0300

    extract validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,11 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const Joi = require('@hapi/joi');
-
-const validationSchema = Joi.object({
-    name: Joi.string().min(2).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-})
+const { DataValidation } = require('../DataValidation');
 
 // app.use('api/user/register'...
 router.post('/register', async (req, res) => {
     // Validate that the body content matches our requirements
-    const { error } = validationSchema.validate(req.body);
+    const { error } = DataValidation.registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const user = new User({
