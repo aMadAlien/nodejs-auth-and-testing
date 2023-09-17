fb5670bb04af2e377c7784b1d8aadec5a79e3b5c
iren <irasibil2@gmail.com>
2023-09-17 17:28:04 +0300

    make robust test for register

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,6 +1,30 @@
-it('returns Register when we hit /register', () => {
-    cy.request('POST', 'http://localhost:3000/api/user/register')
-        .then((res) => {
-            expect(res.body).to.contain('Register');
+
+describe('/user/register', () => {
+    const registerEndpoint = 'http://localhost:3000/api/user/register';
+
+    it('returns 400 when with no body', () => {
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false
         })
+            .then((res) => {
+                expect(res.status).to.eq(400);
+            })
+    });
+
+    it('/register creates a user with valid body', () => {
+        let body = {
+            name: 'John',
+            email: 'test@gmail.com',
+            password: 'qaqaqaq3112'
+        }
+        cy.request('POST', registerEndpoint, body)
+            .then((res) => {
+                expect(res.status).to.eq(200);
+                expect(res.body.name).to.eq('John');
+                expect(res.body.email).to.eq('test@gmail.com');
+                expect(res.body.password).to.eq('qaqaqaq3112');
+            })
+    });
 })
